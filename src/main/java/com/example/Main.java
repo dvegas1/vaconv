5ed4f85a81fb3b14bf2c2412ed07b7c49c5e9a8d
darwin <darwinvegas1@gmail.com>
2018-06-23 23:22:03 -0400

    ULTIMO

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -26,32 +26,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
-@Controller
-@SpringBootApplication
 public class Main {
 
   @Value("${spring.datasource.url}")
   private String dbUrl;
 
   @Autowired
-  private DataSource dataSource;
 
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
+ 
+
 
 
 }
