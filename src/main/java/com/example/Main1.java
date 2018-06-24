77a46ba8df60bfae041913c91daadcae5ef5416e
darwin <darwinvegas1@gmail.com>
2018-06-23 21:09:14 -0400

    ULTIMO

---
+++ src/main/java/com/example/Main1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ * @author dvegas
+ */
+public class Main1 {
+    
+      public static void main(String[] args) throws Exception {
+          System.out.println("hola");
+  }
+}
