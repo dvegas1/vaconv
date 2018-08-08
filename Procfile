89a665356cd8f5f577072a343d4f3fb55ea94121
darwin <darwinvegas1@gmail.com>
2018-08-08 15:02:24 -0400

    ultimo08082018

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java -jar target/traprange-1-spring-boot.jar -in "_Docs/sample-1.pdf" -out "result/idx.html" -el "0,1,-1"
+java -jar target/traprange-1-spring-boot.jar -in _Docs/sample-1.pdf -out result/idx.html -el 0,1,-1
 
--- test-command-line.sh
+++ test-command-line.sh
@@ -4,7 +4,7 @@ rm -rf $home/result/*
 
 # Sample 1
 idx=1
-java -jar traprange.lastest.jar -in "$home/sample-$idx.pdf" -out "$home/result/sample-$idx.html" -el "0,1,-1"
+java -jar $home/traprange.lastest.jar -in "$home/sample-$idx.pdf" -out "$home/result/sample-$idx.html" -el "0,1,-1"
 
 # Sample 2
 idx=2
